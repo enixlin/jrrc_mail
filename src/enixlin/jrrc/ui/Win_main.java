6a3a72877e8c1d98476fb1275a73d9b0a9fdcef3
enixlin <enixlin@DESKTOP-0DM3F3R>
2015-11-18 23:01:34 +0800

    gh

---
+++ src/enixlin/jrrc/ui/Win_main.java
@@ -0,0 +1,43 @@
+package enixlin.jrrc.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Win_main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Win_main window = new Win_main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Win_main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
