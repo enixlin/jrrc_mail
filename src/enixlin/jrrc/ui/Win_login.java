a1020594727efaecd6b3ca63cb08652cff1a947a
enixlin <enixlin@DESKTOP-0DM3F3R>
2015-11-23 22:29:35 +0800

    var

--- src/enixlin/jrrc/ui/Win_login.java
+++ src/enixlin/jrrc/ui/Win_login.java
@@ -12,6 +12,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -140,15 +141,18 @@ public class Win_login {
 				ScriptEngineManager manager = new ScriptEngineManager();  
 				 ScriptEngine engine = manager.getEngineByName("javascript");
 				
-				 String script = "var obj = 'enixlin'";
+				 String script="function NodeData(type,name,level,id,departmentid){this.type=type;this.name=name;this.level=level;this.id=id;this.departmentid=departmentid;}  "
+				 		+ " var d0=new NodeData(0,'广东省农村信用社','d','0'); var d1=new NodeData(1,'省联社',1,'2579',d0);var d2=new NodeData(2,'省联社领导',1,'2079',d1);var d9=new NodeData(9,'顾问',1,'9226',d1);var d11=new NodeData(11,'主任助理',1,'3081',d1);var d13=new NodeData(13,'专家',1,'9442',d1);var d16=new NodeData(16,'调研员',1,'6529',d1);var d17=new NodeData(17,'巡视组',1,'9656',d1);var d19=new NodeData(19,'办公室',1,'2080',d1);var d20=new NodeData(20,'主任室',1,'10280',d19);var d22=new NodeData(22,'综合室',1,'10277',d19);var d27=new NodeData(27,'秘书室',1,'10278',d19);var d32=new NodeData(32,'文印室',1,'10279',d19);var d38=new NodeData(38,'基建',1,'10281',d19);var d62=new NodeData(62,'人力资源部',1,'2081',d1);var d85=new NodeData(85,'改革与战略研究部',1,'2085',d1);var d92=new NodeData(92,'计划资金财务部',1,'2083',d1);var d107=new NodeData(107,'资金调剂营运中心',1,'7597',d1);var d108=new NodeData(108,'总经理室',1,'8031',d107);var d112=new NodeData(112,'投资业务室',1,'8032',d107);var d120=new NodeData(120,'风险控制室',1,'8033',d107);var d125=new NodeData(125,'资金清算室',1,'8034',d107);var d131=new NodeData(131,'理财服务室(理财服务中心)',1,'8035',d107);";
+				 
+				 
 			        try {
 						engine.eval(script);
 					} catch (ScriptException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}   
-			        Object obj = engine.get("obj");   
-			        System.out.println(obj.toString());
+			        Object obj = engine.get("d1");   
+			        System.out.println(obj);
 			}
 		});
 		btnNewButton_1.setBounds(234, 153, 93, 23);
