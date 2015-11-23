a080f788e9a50b6d147e4d4da8342590f426a08d
enixlin <enixlin1981>
2015-11-23 09:10:55 +0800

    model

--- src/enixlin/jrrc/model/TableModel.java
+++ src/enixlin/jrrc/model/TableModel.java
@@ -1,5 +1,25 @@
 package enixlin.jrrc.model;
 
-public class TableModel {
+import javax.swing.table.AbstractTableModel;
+
+public class TableModel extends AbstractTableModel {
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
